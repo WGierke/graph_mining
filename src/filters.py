44ab528d271dce4a49937147af8de4caf078e72a
Fabian Paul <fabian.paul@student.hpi.de>
2017-12-14 23:52:40 +0100

    Use filter arguments for user subset

--- src/filters.py
+++ src/filters.py
@@ -1,6 +1,6 @@
-def use_n_most_occuring_users(df, n):
+def use_n_most_occuring_users(df, n=100):
     users = df.groupby(df['by']).size()
     filtered_users, _ = zip(
-        *users.sort_values(ascending=False).head(100).items())
+        *users.sort_values(ascending=False).head(n).items())
     unique_users = set(filtered_users)
     return df[df['by'].map(lambda x: x in unique_users)]
