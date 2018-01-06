de410eb15c6d5a70651792c45a8619c77deddef9
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2018-01-06 16:25:39 +0100

    Add graph building pipeline

--- src/filters.py
+++ src/filters.py
@@ -2,5 +2,10 @@ def use_n_most_occuring_users(df, n=100):
     users = df.groupby(df['by']).size()
     filtered_users, _ = zip(
         *users.sort_values(ascending=False).head(n).items())
-    unique_users = set(filtered_users)
-    return df[df['by'].map(lambda x: x in unique_users)]
+    return df[df['by'].map(lambda x: x in filtered_users)]
+
+
+def get_users_with_more_than_n_comments(df, n=10):
+    users = df.groupby(df['by']).size()
+    users, _ = zip(*users[users > n].items())
+    return df[df['by'].map(lambda x: x in users)]
