8674da65d2ac32509722a44ed044c9ac2b6b487d
Willi Gierke <willi.gierke@student.hpi.de>
2018-01-28 23:56:33 +0100

    Add text filtering

--- src/graphbuilder.py
+++ src/graphbuilder.py
@@ -9,15 +9,16 @@ class GraphBuilder:
         self.graph = Graph()
         self.author_index = set()
 
-    def _add_non_existent_node(self, author):
+    def _add_non_existent_node(self, author, topic):
         if author not in self.author_index:
             self.author_index.add(author)
-            self.graph.add_vertices(author)
+            self.graph.add_vertex(author, label=topic)
 
     def extend_graph(self, df):
         for _, row in tqdm(df.iterrows()):
             author = row['by']
-            self._add_non_existent_node(author)
+            topic = row['topic']
+            self._add_non_existent_node(author, topic)
             parent = row['parent']
             if math.isnan(parent):
                 continue
@@ -25,5 +26,6 @@ class GraphBuilder:
             if len(parent_df) == 0:
                 continue
             parent_author = parent_df.iloc[0]['by']
-            self._add_non_existent_node(parent_author)
+            parent_topic = parent_df.iloc[0]['topic']
+            self._add_non_existent_node(parent_author, parent_topic)
             self.graph.add_edges([(author, parent_author)])
